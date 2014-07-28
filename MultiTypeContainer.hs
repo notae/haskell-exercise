1228d113426e674bf9940f64b097ba90890f6d7b
notae <notae@me.com>
2014-07-29 01:19:04 +0900

    Example of Container for Multiple Types

---
+++ MultiTypeContainer.hs
@@ -0,0 +1,77 @@
+-- Example of Container for Multiple Types
+
+{-# LANGUAGE RankNTypes #-}
+
+module MultiTypeContainer where
+
+import Control.Applicative (Applicative)
+import Control.Applicative ((<$>))
+import Control.Applicative ((<*>))
+import Control.Applicative (WrappedMonad (..))
+import Data.Maybe (listToMaybe)
+import Data.Traversable (traverse)
+
+class Container c where
+  cmap :: (forall a. t a -> t' a) -> c t -> c t'
+  cmapA :: Applicative f => (forall a. t a -> f (t' a)) -> c t -> f (c t')
+  cmapM :: Monad m => (forall a. t a -> m (t' a)) -> c t -> m (c t')
+  cmapM f = unwrapMonad . cmapA (WrapMonad . f)
+
+-- containing 2 types
+
+newtype Pair x y t = Pair { unPair :: (t x, t y) }
+                     deriving (Show, Eq)
+
+instance Container (Pair x y) where
+  cmap f (Pair (tx, ty)) = Pair (f tx, f ty)
+  cmapA f (Pair (tx, ty)) = (\x y -> Pair (x,y)) <$> (f tx) <*> (f ty)
+
+pair :: Pair Int Bool []
+pair = Pair ([1..3], [True, False])
+
+testPairCmap :: Pair Int Bool Maybe
+testPairCmap  = cmap listToMaybe pair
+
+testPairCmapA :: [Pair Int Bool Maybe]
+testPairCmapA = cmapA (map Just) pair
+
+{-|
+>>> testPairCmapM
+[Pair {unPair = (Just 1,Just True)},Pair {unPair = (Just 1,Just False)},Pair {unPair = (Just 2,Just True)},Pair {unPair = (Just 2,Just False)},Pair {unPair = (Just 3,Just True)},Pair {unPair = (Just 3,Just False)}]
+-}
+testPairCmapM :: [Pair Int Bool Maybe]
+testPairCmapM = cmapM (map Just) pair
+
+-- containg container
+
+data ListC a b c t =
+  ListC
+  { unListCA :: [t a]
+  , unListCB :: [t b]
+  , unListCC :: [t c] }
+  deriving (Show)
+
+instance Container (ListC a b c) where
+  cmap  f (ListC as bs cs) = ListC (fmap f as) (fmap f bs) (fmap f cs)
+  cmapA f (ListC as bs cs) =
+    ListC <$> (traverse f as) <*> (traverse f bs) <*> (traverse f cs)
+
+listc :: ListC Int Bool Ordering []
+listc = ListC [[1..3], [4..5]] [[True, False]] [[LT, GT]]
+
+testListCCmap :: ListC Int Bool Ordering Maybe
+testListCCmap  = cmap listToMaybe listc
+
+testListCCmapA :: [ListC Int Bool Ordering Maybe]
+testListCCmapA = cmapA (map Just) listc
+
+{-|
+>>> length $ testListCCmapM
+24
+>>> head $ testListCCmapM
+ListC {unListCA = [Just 1,Just 4], unListCB = [Just True], unListCC = [Just LT]}
+>>> last $ testListCCmapM
+ListC {unListCA = [Just 3,Just 5], unListCB = [Just False], unListCC = [Just GT]}
+-}
+testListCCmapM :: [ListC Int Bool Ordering Maybe]
+testListCCmapM = cmapM (map Just) listc
