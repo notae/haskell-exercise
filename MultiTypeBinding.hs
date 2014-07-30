d3bac9d75cd0a2dc47aeabf698a9686ad3bdc33e
notae <notae@me.com>
2014-07-30 19:37:03 +0900

    Example of Multiple Type Binding
    remove unneeded constarint from class Binding
    toward supporing ST monad.

--- MultiTypeBinding.hs
+++ MultiTypeBinding.hs
@@ -1,4 +1,4 @@
--- Example of Multiple Type Binding over MonadState
+-- Example of Multiple Type Binding
 
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -17,15 +17,19 @@ import qualified Data.IntMap as IntMap
 import Data.Set (Set)
 import qualified Data.Set as Set
 
--- Interface for variable binding over MonadState
+-- Interface for variable binding
 
-class MonadState s m => Binding m s v where
-  type family Var m s v :: *
-  newVar    :: v         -> m (Var m s v)
-  lookupVar :: Var m s v -> m v
-  updateVar :: Var m s v -> v -> m ()
+class Monad m => Binding m v where
+  type family Var m v :: *
+  newVar    :: v       -> m (Var m v)
+  lookupVar :: Var m v -> m v
+  updateVar :: Var m v -> v -> m ()
 
--- An implementation of binding with IntMap
+-- An implementation of binding with State monad + IntMap
+
+type IMVarId = IntMap.Key
+
+data IMVar v = IMVar { imVarId :: IMVarId } deriving (Show, Eq)
 
 data IMEnv =
   IMEnv
@@ -34,8 +38,6 @@ data IMEnv =
   , imBMap :: IntMap Bool }
   deriving (Show)
 
-data IMVar v = IMVar { imVarId :: IMVarId } deriving (Show, Eq)
-
 initIMEnv :: IMEnv
 initIMEnv =
   IMEnv
@@ -43,14 +45,12 @@ initIMEnv =
   , imIMap    = IntMap.empty
   , imBMap    = IntMap.empty }
 
-type IMVarId = IntMap.Key
-
 class MonadState IMEnv m => IMBinding m v where
   getMap :: m (IntMap v)
   setMap :: IntMap v -> m ()
 
-instance IMBinding m v => Binding m IMEnv v where
-  type Var m IMEnv v = IMVar v
+instance IMBinding m v => Binding m v where
+  type Var m v = IMVar v
   newVar v = do
     vid <- gets imNextId
     modify $ \s -> s { imNextId = vid + 1 }
@@ -76,8 +76,8 @@ instance MonadState IMEnv m => IMBinding m Bool where
 
 -- Example for use
 
-prog :: State IMEnv ((Int, Bool), (Int, Bool))
-prog = do
+progIM :: State IMEnv ((Int, Bool), (Int, Bool))
+progIM = do
   vi <- newVar 123
   vb <- newVar True
   i1 <- lookupVar vi
@@ -89,8 +89,18 @@ prog = do
   return ((i1, b1), (i2, b2))
 
 {-|
->>> test
+>>> testIM
 (((123,True),(456,False)),IMEnv {imNextId = 2, imIMap = fromList [(0,456)], imBMap = fromList [(1,False)]})
 -}
-test :: (((Int, Bool), (Int, Bool)), IMEnv)
-test = runState prog initIMEnv
+testIM :: (((Int, Bool), (Int, Bool)), IMEnv)
+testIM = runState progIM initIMEnv
+
+progIM2New :: State IMEnv (IMVar Int)
+progIM2New = newVar 123
+
+progIM2Use :: IMVar Int -> State IMEnv Int
+progIM2Use v = lookupVar v
+
+-- Should causes type error
+testIM2 :: (Int, IMEnv)
+testIM2 = runState (progIM2Use (evalState progIM2New initIMEnv)) initIMEnv
