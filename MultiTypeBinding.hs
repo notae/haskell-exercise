33931ffc3759eaf176326d77539a0343d90b3010
notae <notae@me.com>
2014-08-02 10:20:42 +0900

    Example of Multiple Type Binding
    add predefined instances of class IMEnvId, IMEnvMap
    with fixed number of type parameters.

--- MultiTypeBinding.hs
+++ MultiTypeBinding.hs
@@ -84,7 +84,7 @@ runIM im = runStateT (unIM im)
 evalIM :: Monad m => (forall s. IM e m s a) -> e -> m a
 evalIM im = evalStateT (unIM im)
 
---   Declaration per type
+--   Declaration per type (with aconcrete types)
 
 data MyIMEnv =
   MyIMEnv
@@ -144,6 +144,40 @@ instance IMEnvMap MyIMEnv [Color] where
   getMap = imCLMap
   setMap e m = e { imCLMap = m }
 
+--   Declaration per type (with 2 type parameters)
+
+data MyIM2Env v1 v2 =
+  MyIM2Env
+  { im2NextId :: IMVarId
+  , im2Map1   :: IntMap v1
+  , im2Map2   :: IntMap v2 }
+  deriving (Show)
+
+initialMyIM2Env :: MyIM2Env v1 v2
+initialMyIM2Env =
+  MyIM2Env
+  { im2NextId = 0
+  , im2Map1   = IntMap.empty
+  , im2Map2   = IntMap.empty }
+
+runMyIM20 :: (forall s. IM (MyIM2Env v1 v2) m s a) -> m (a, MyIM2Env v1 v2)
+runMyIM20 im = runIM im initialMyIM2Env
+
+evalMyIM20 :: Monad m => (forall s. IM (MyIM2Env v1 v2) m s a) -> m a
+evalMyIM20 im = evalIM im initialMyIM2Env
+
+instance IMEnvId (MyIM2Env v1 v2) where
+  getId = im2NextId
+  setId e vid = e { im2NextId = vid }
+
+instance IMEnvMap (MyIM2Env v1 v2) v1 where
+  getMap = im2Map1
+  setMap e m = e { im2Map1 = m }
+
+instance IMEnvMap (MyIM2Env v1 v2) v2 where
+  getMap = im2Map2
+  setMap e m = e { im2Map2 = m }
+
 -- Example for use
 
 prog :: (Binding m s Bool, Binding m s Color) =>
@@ -187,6 +221,23 @@ testIML = evalMyIM0 $ do
   b <- IM $ lift lb
   c <- IM $ lift lc
   return (b, c)
+
+-- with predefined instances of class IMEnvId, IMEnvMap
+
+progIM2 :: (Binding m e Bool, Binding m e Color) => m e (Bool, Color)
+progIM2 = do
+  vb <- newVar True
+  vc <- newVar Green
+  b <- lookupVar vb
+  c <- lookupVar vc
+  return (b, c)
+
+{-|
+>>> testIM2
+((True,Green),MyIM2Env {im2NextId = 2, im2Map1 = fromList [(0,True)], im2Map2 = fromList [(1,Green)]})
+-}
+testIM2 :: ((Bool, Color), MyIM2Env Bool Color)
+testIM2 = runIdentity $ runMyIM20 progIM2
 
 -- Type check with phantom type parameter
 
