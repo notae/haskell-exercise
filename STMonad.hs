0d93245b8d952281abaf88db6ee4b28838508222
notae <notae@me.com>
2014-08-22 16:00:44 +0900

    ST Monad Example
    add an example of multiple type stacks with global rewinding.

--- STMonad.hs
+++ STMonad.hs
@@ -12,6 +12,8 @@ import qualified Data.Traversable as Traversable
 import Data.Set (Set)
 import Data.Set (deleteMin)
 import qualified Data.Set as Set
+import Control.Monad (replicateM_)
+import Data.STRef (writeSTRef)
 
 prog :: Int -> ST s Int
 prog i = do
@@ -40,21 +42,88 @@ progT t = runST $ do
   -- export from ST
   Traversable.mapM readSTRef tr
 
--- with List Monad
+-- with Set and List Monad
 
-{-
-label :: STRef s [a] -> ST s [a]
-label = do
-  val <- 
-
-progL :: (Traversable t, Num a) => t (Set a) -> [t a]
-progL t = runST $ do
+propagate :: (Traversable t, Num a) => t (Set a) -> t (Set a)
+propagate t = runST $ do
   -- import into ST
   tr  <- Traversable.mapM newSTRef t
   -- do something
   _   <- Traversable.mapM (flip modifySTRef deleteMin) tr
   -- export from ST
   tr' <- Traversable.mapM readSTRef tr
+  return tr'
+
+-- label :: STRef s [a] -> ST s [a]
+-- label = undefined
+
+label :: Set a -> [a]
+label s = Set.toList s
+
+labelling :: (Traversable t, Num a) => t (Set a) -> [t a]
+labelling ts = Traversable.mapM label ts
+
+
+dup :: STRef s a -> ST s (STRef s a)
+dup r = do
+  v <- readSTRef r
+  newSTRef v
+
+progL :: ST s [[Int]]
+progL = do
+  rs <- Traversable.mapM newSTRef [[1..3], [1..5]]
+  -- duplicate
+  rs' <- Traversable.mapM (\r -> readSTRef r >>= newSTRef) rs
   -- labelling
-  Traversable.mapM label tr'
--}
+  Traversable.mapM readSTRef rs'
+  -- export
+  -- vs' <- Traversable.mapM readSTRef rs'
+  -- return vs'
+
+
+-- stack
+
+type Manager s = STRef s (ST s ())
+
+data Stack s a = Stack { unStack :: [a]
+                       , manager :: Manager s }
+emptyStack :: Manager s -> Stack s a
+emptyStack m = Stack [] m
+
+newManager :: ST s (Manager s)
+newManager = newSTRef (return ())
+
+new :: Manager s -> ST s (STRef s (Stack s a))
+new m = newSTRef (emptyStack m)
+
+push :: STRef s (Stack s a) -> a -> ST s ()
+push r a = do
+  s <- readSTRef r
+  writeSTRef r s { unStack = a:(unStack s) }
+  let rm = manager s
+  modifySTRef rm $ \m -> m >> pop r >> return ()
+
+pop :: STRef s (Stack s a) -> ST s a
+pop r = do
+  Stack (a:as) _ <- readSTRef r
+  modifySTRef r $ \s -> s { unStack = as }
+  return a
+
+rewind :: Manager s -> ST s ()
+rewind rm = do
+  m <- readSTRef rm
+  m
+
+testStack :: ([Int], [Bool])
+testStack = runST $ do
+  m <- newManager
+  i <- new m
+  b <- new m
+  push i 123
+  push b True
+  push i 456
+  push b False
+  rewind m
+  Stack asi _ <- readSTRef i
+  Stack asb _ <- readSTRef b
+  return (asi, asb)
