cda3f90f1e6ed8dfa149b6a426e3f4be377a8b06
notae <notae@me.com>
2014-08-24 14:49:04 +0900

    Example of Multiple Type Stack on ST Monad
    Separate current value and stack.
    Add pop operation.

--- MultiTypeStack.hs
+++ MultiTypeStack.hs
@@ -11,53 +11,90 @@ import Data.STRef (STRef)
 import Data.STRef (modifySTRef)
 import Data.STRef (newSTRef)
 import Data.STRef (readSTRef)
+import Data.STRef (writeSTRef)
 
 
-data Stack s = forall a. (Show a) => Stack (STRef s [a])
+type Var s a = STRef s a
 
-sShow :: Stack s -> ST s String
-sShow (Stack r) = liftM show $ readSTRef r
+data VarInfo s a = VarInfo { var   :: Var s a
+                           , stack :: STRef s [a] }
 
-new :: Show a => STRef s [Stack s] -> a -> ST s (STRef s [a])
-new rvs a = do
-  r <- newSTRef [a]
-  modifySTRef rvs $ \vs -> Stack r:vs
-  return r
+data AnyVarInfo s = forall a. (Show a) => AnyVarInfo (VarInfo s a)
 
-get :: STRef s [a] -> ST s a
-get r = do
-  (a:_) <- readSTRef r
-  return a
+type VarsInfo s = STRef s [AnyVarInfo s]
 
-set :: STRef s [a] -> a -> ST s ()
-set r a = modifySTRef r $ \(_:as) -> a:as
+newVarsInfo :: ST s (VarsInfo s)
+newVarsInfo = newSTRef []
 
-push :: Stack s -> ST s ()
-push (Stack r) = modifySTRef r $ \as@(a:_) -> a:as
+sShow :: AnyVarInfo s -> ST s String
+sShow (AnyVarInfo vi) = liftM show $ readSTRef (var vi)
 
-pushs :: STRef s [Stack s] -> ST s ()
-pushs rvs = do
-  vs <- readSTRef rvs
-  mapM_ push vs
+_new :: Show a => VarsInfo s -> a -> ST s (Var s a)
+_new vsi a = do
+  v <- newSTRef a
+  s <- newSTRef []
+  modifySTRef vsi $ \vis -> AnyVarInfo (VarInfo v s):vis
+  return v
 
-testStack :: ([Int], [Bool], [Ordering], [String])
+get :: Var s a -> ST s a
+get = readSTRef
+
+set :: Var s a -> a -> ST s ()
+set = writeSTRef
+
+_push :: VarsInfo s -> ST s ()
+_push vsi = do
+  vis <- readSTRef vsi
+  mapM_ __push vis
+  where
+    __push :: AnyVarInfo s -> ST s ()
+    __push (AnyVarInfo (VarInfo v s)) = do
+      a <- readSTRef v
+      modifySTRef s $ \as -> a:as
+
+_pop :: VarsInfo s -> ST s ()
+_pop vsi = do
+  vis <- readSTRef vsi
+  mapM_ __pop vis
+  where
+    __pop :: AnyVarInfo s -> ST s ()
+    __pop (AnyVarInfo (VarInfo v s)) = do
+      (a:as) <- readSTRef s
+      writeSTRef v a
+      writeSTRef s as
+
+
+{-|
+>>> testStack
+(123,True,LT,["GT","False","456"],["LT","True","123"])
+-}
+testStack :: (Int, Bool, Ordering, [String], [String])
 testStack = runST $ do
-  rvs <- newSTRef []
-  let newr a = new rvs a
-  -- create stacks with initial values
-  ri <- newr 123
-  rb <- newr True
-  ro <- newr LT
-  -- push all stacks
-  pushs rvs
-  -- modify value at the top of stacks
-  set ri 456
-  set rb False
-  set ro GT
+  -- setup
+  vsi <- newVarsInfo
+  let new a = _new vsi a
+  let push = _push vsi
+  let pop = _pop vsi
+  -- create variables with stack and initial values
+  vi <- new 123
+  vb <- new True
+  vo <- new LT
+  -- push all variables
+  push
+  -- modify variables
+  set vi 456
+  set vb False
+  set vo GT
+  -- (for debug)
+  vis <- readSTRef vsi
+  ls <- mapM sShow vis
+  -- pop all variables
+  pop
   -- get results
-  li <- readSTRef ri
-  lb <- readSTRef rb
-  lo <- readSTRef ro
-  ss <- readSTRef rvs
-  ls <- mapM sShow ss
-  return (li, lb, lo, ls)
+  i <- get vi
+  b <- get vb
+  o <- get vo
+  -- (for debug)
+  vis' <- readSTRef vsi
+  ls' <- mapM sShow vis'
+  return (i, b, o, ls, ls')
