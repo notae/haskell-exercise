71ac69431bc0cdeceb535aa3f309dfff555aeeea
notae <notae@me.com>
2014-09-08 23:45:16 +0900

    Example of 2-List Queue
    Added predicate 'qnull'.

--- Queue.hs
+++ Queue.hs
@@ -12,16 +12,25 @@ empty :: Queue a
 empty = Q [] []
 
 {-|
+>>> qnull empty
+True
+>>> qnull $ enq 1 empty
+False
+-}
+qnull :: Queue a -> Bool
+qnull (Q is os) = null is && null os
+
+{-|
 >>> enq 2 $ enq 1 empty
 Q [2,1] []
 -}
 enq :: a -> Queue a -> Queue a
-enq a (Q ins outs) = Q (a:ins) outs
+enq a (Q is os) = Q (a:is) os
 
 {-|
 >>> deq $ enq 2 $ enq 1 empty
 (1,Q [] [2])
 -}
 deq :: Queue a -> (a, Queue a)
-deq (Q ins [])       = deq $ Q [] $ reverse ins
-deq (Q ins (a:outs)) = (a, Q ins outs)
+deq (Q is [])     = deq $ Q [] $ reverse is
+deq (Q is (a:os)) = (a, Q is os)
