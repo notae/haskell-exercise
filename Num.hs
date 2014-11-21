c46a4e0ac7bd24cd6300001d0715de5eb12b5c9f
notae <notae@me.com>
2014-11-21 23:42:41 +0900

    Define (*) for Expr

--- Num.hs
+++ Num.hs
@@ -6,17 +6,20 @@ data Expr
   = Value Int
   | Var String
   | Expr :+ Expr
+  | Expr :* Expr
   deriving (Show, Read, Eq)
 
 instance Num Expr where
   a + b = a :+ b
+  a * b = a :* b
   fromInteger i = Value (fromInteger i)
 
 {-|
->>> eval [("x", 10)] (Var "x" + 1)
-11
+>>> eval [("x", 10), ("y", 3)] (Var "x" + Var "y" * 2)
+16
 -}
 eval :: [(String, Int)] -> Expr -> Int
 eval _ (Value i) = i
 eval e (Var s) = fromMaybe 0 (lookup s e)
 eval e (x :+ y) = eval e x + eval e y
+eval e (x :* y) = eval e x * eval e y
