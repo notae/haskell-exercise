b5240c22783708955e805c0b25c10e46a897d7ea
notae <notae@me.com>
2014-11-29 23:36:30 +0900

    Close server socket on exception.

--- EchoServer.hs
+++ EchoServer.hs
@@ -1,20 +1,19 @@
+-- Example of echo server
+
 module EchoServer where
 
-import Network.Socket
-import Control.Concurrent
-import System.IO
 import Control.Applicative ((<$>))
+import Control.Concurrent
+import Control.Exception
+import Network.Socket
+import System.IO
 
 echoServer :: ServiceName -> IO ()
 echoServer port = withSocketsDo $ do
   serverAddr <- getaddrinfo port
-  sock <- socket (addrFamily serverAddr)
-                 (addrSocketType serverAddr)
-                 defaultProtocol
-  bindSocket sock (addrAddress serverAddr)
-  listen sock 5
-  lock <- newMVar ()
-  procRequests lock sock
+  bracket (listenOn serverAddr) close $ \sock -> do
+    lock <- newMVar ()
+    procRequests lock sock
 
 getaddrinfo :: ServiceName -> IO AddrInfo
 getaddrinfo port =
@@ -22,6 +21,15 @@ getaddrinfo port =
                                         -- , addrFamily = AF_INET
                                            , addrSocketType = Stream }))
                        Nothing (Just port)
+
+listenOn :: AddrInfo -> IO Socket
+listenOn serverAddr = do
+  sock <- socket (addrFamily serverAddr)
+                 (addrSocketType serverAddr)
+                 defaultProtocol
+  bindSocket sock (addrAddress serverAddr)
+  listen sock 5
+  return sock
 
 procRequests :: MVar () -> Socket -> IO ()
 procRequests lock masterSock = do
@@ -33,14 +41,14 @@ procMessages :: MVar () -> Socket -> SockAddr -> IO ()
 procMessages lock connSock clientAddr = do
   connhdl <- socketToHandle connSock ReadWriteMode
   hSetBuffering connhdl LineBuffering
-  handle lock connhdl clientAddr "echoServer: client connected."
+  handleMessage lock connhdl clientAddr "echoServer: client connected."
   messages <- hGetContents connhdl
-  mapM_ (handle lock connhdl clientAddr) (lines messages)
-  handle lock connhdl clientAddr "echoServer: client disconnected."
+  mapM_ (handleMessage lock connhdl clientAddr) (lines messages)
+  handleMessage lock connhdl clientAddr "echoServer: client disconnected."
   hClose connhdl
 
-handle :: MVar () -> Handle -> SockAddr -> String -> IO ()
-handle lock connhdl clientAddr msg = do
+handleMessage :: MVar () -> Handle -> SockAddr -> String -> IO ()
+handleMessage lock connhdl clientAddr msg = do
   hPutStrLn connhdl msg
   withMVar lock (\a -> handlerFunc clientAddr msg >> return a)
 
