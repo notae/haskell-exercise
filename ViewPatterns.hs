6bdab05c744f64a9a3abc3f408b659737c5c02c9
notae <notae@me.com>
2014-12-15 03:32:39 +0900

    Example of ViewPatterns extension.

---
+++ ViewPatterns.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ViewPatterns    #-}
+
+module ViewPatterns where
+
+data D = D { dFirst :: Int, dSecond :: Bool } deriving (Show, Eq)
+
+view :: D -> (Int, Bool)
+view D{..} = (dFirst, dSecond)
+
+-- GHC says "Warning: Pattern match(es) are non-exhaustive"
+f :: D -> Int
+f (view -> (i, True)) = i * 2
+f (view -> (i, False)) = i
+
+-- no warning
+g :: (Int, Bool) -> Int
+g (i, True) = i * 2
+g (i, False) = i
