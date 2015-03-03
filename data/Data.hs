1f8ed8b7d5f3cbefe174a6a05fd350a62adaf166
notae <notae@me.com>
2015-03-04 06:49:24 +0900

    Modified the example for Data.Data

--- data/Data.hs
+++ data/Data.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveDataTypeable  #-}
+{-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Data where
@@ -15,13 +16,17 @@ data X =
 x :: X
 x = X 123 'a'
 
-f :: forall d. Data d => d -> d
-f d = case cast d of
-       Nothing          -> d
-       Just (i :: Int)  -> fromJust (cast (i + 1))
+incr :: forall d. Data d => d -> d
+incr d = case cast d of
+          Nothing          -> d
+          Just (i :: Int)  -> fromJust (cast (i + 1))
 
+{-|
+>>> test
+X {foo = 124, bar = 'a'}
+-}
 test :: X
-test = gmapT f x
+test = gmapT incr x
 
 data Y =
   Y
@@ -33,17 +38,45 @@ data Y =
 y :: Y
 y = Y True 456 x
 
+{-|
+>>> test2
+Y {baz = True, qux = 457, yx = X {foo = 123, bar = 'a'}}
+-}
 test2 :: Y
-test2 = gmapT f y
+test2 = gmapT incr y
+
+{-|
+>>> test3
+Y {baz = True, qux = 456, yx = X {foo = 124, bar = 'a'}}
+-}
 test3 :: Y
-test3 = gmapT (gmapT f) y
+test3 = gmapT (gmapT incr) y
+
+{-|
+>>> test4
+Y {baz = True, qux = 457, yx = X {foo = 124, bar = 'a'}}
+-}
 test4 :: Y
-test4 = gmapT (gmapT f) (gmapT f y)
+test4 = gmapT (gmapT incr) (gmapT incr y)
 
-f' :: forall d. Data d => d -> d
-f' d = case cast d of
-       Nothing          -> gmapT f' d
-       Just (i :: Int)  -> fromJust (cast (i + 1))
+incr' :: forall d. Data d => d -> d
+incr' d = case cast d of
+           Nothing          -> gmapT incr' d
+           Just (i :: Int)  -> fromJust (cast (i + 1))
 
+{-|
+>>> test5
+Y {baz = True, qux = 457, yx = X {foo = 124, bar = 'a'}}
+-}
 test5 :: Y
-test5 = gmapT f' y
+test5 = gmapT incr' y
+
+gmapT' :: Data a => (forall b. Data b => b -> b) -> a -> a
+gmapT' f = gmapT (f . gmapT f)
+
+{-|
+>>> test6
+Y {baz = True, qux = 457, yx = X {foo = 124, bar = 'a'}}
+-}
+test6 :: Y
+test6 = gmapT' incr y
