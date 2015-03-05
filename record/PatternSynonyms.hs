94ee027f79865357a4afccd234c7421b42cb287f
notae <notae@me.com>
2015-03-05 22:15:23 +0900

    Added an example for PatternSynonyms.

---
+++ record/PatternSynonyms.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE PatternSynonyms #-}
+
+module PatternSynonyms where
+
+-- defined as pattern
+pattern P1 = 1
+pattern P2 = 2
+-- just covered
+f :: Int -> String
+f P1 = "one"
+f P2 = "two"
+f _  = "unknown"
+
+-- defined as variables for comarison
+p1 = 1
+p2 = 2
+-- causes "Warning: Pattern match(es) are overlapped"
+g :: Int -> String
+g p1 = "one"
+g p2 = "two"
+g _  = "unknown"
+
+-- defined as pattern with tupple
+pattern F x y = (x, y)
+-- causes "Warning: Pattern match(es) are non-exhaustive"
+h :: (Int, Int) -> Int
+h (F 2 3) = 999
+h (F x y) = x + y
+
+-- defined without pattern synonyms for comparison
+-- causes no warnings
+h' :: (Int, Int) -> Int
+h' (2, 3) = 999
+h' (x, y) = x + y
+-- an similar example
+fn :: Maybe Int -> Int
+fn (Just n) = n
+fn Nothing  = -1
+
+-- unidirectional pattern
+pattern Head x <- x:xs
+head' :: [a] -> Maybe a
+head' (Head x) = Just x
+head' _        = Nothing
