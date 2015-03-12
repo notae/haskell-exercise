dff286292acb3e9ecdfe58f9f27d6ec90fa4471d
notae <notae@me.com>
2015-03-13 02:43:04 +0900

    Added examples from FOP 12.3

--- fop/Fop12_3.hs
+++ fop/Fop12_3.hs
@@ -4,6 +4,8 @@
 
 module Fop12_3 where
 
+import Control.Monad (liftM, liftM2)
+
 data Type t where
   RDyn ∷ Type Dynamic
   RInt ∷ Type Int
@@ -25,7 +27,7 @@ instance Show (Type t) where
 
 data Dynamic = ∀ τ. Dyn (Type τ) τ
 
--- for debug
+-- show only type for debug
 instance Show Dynamic where
   show (Dyn ra _) = show ra
 
@@ -40,3 +42,24 @@ ds = [Dyn RInt 60, Dyn rString "Bird"]
 
 dds :: Dynamic
 dds = Dyn (RList RDyn) ds
+
+teq ∷ ∀τ σ . Type τ → Type σ → Maybe (τ → σ)
+teq (RInt) (RInt) = return id
+teq (RChar) (RChar) = return id
+teq (RList ra1) (RList ra2) = liftM fmap (teq ra1 ra2)
+teq (RPair ra1 rb1) (RPair ra2 rb2) = liftM2 (><) (teq ra1 ra2) (teq rb1 rb2)
+  where (><) f g (a, b) = (f a, g b)
+teq _ _ = Nothing
+
+{-|
+>>> cast (Dyn RInt 123) RInt
+Just 123
+>>> cast (Dyn RInt 123) RChar
+Nothing
+>>> cast (Dyn rString "foo") rString
+Just "foo"
+>>> cast (Dyn (RPair RInt rString) (123,"foo")) (RPair RInt rString)
+Just (123,"foo")
+-}
+cast ∷ ∀τ. Dynamic → Type τ → Maybe τ
+cast (Dyn ra a) rt = fmap (\f → f a) (teq ra rt)
