2c7d020be6c8df7faa4b159b6c2fef56311c9879
notae <notae@me.com>
2015-03-27 04:42:40 +0900

    Added examples for generic programming.

--- pack/Functor.hs
+++ pack/Functor.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE DeriveFunctor              #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes                 #-}
 {-# LANGUAGE StandaloneDeriving         #-}
 
 module Functor where
 
 import Data.Functor
--- import Data.Functor.Classes
 import Data.Functor.Compose
+import Data.Functor.Constant
 import Data.Functor.Identity
 import Data.Functor.Product
 import Data.Maybe
@@ -33,17 +34,37 @@ instance Functor (PA b) where
   fmap f (PA (P (a, b))) = PA (P (f a, b))
 
 --
--- Compose
+-- Data.Functor.Compose
 --
 {-|
->>> getCompose $ fmap (+1) $ Compose [Just 1, Nothing, Just 3]
+>>> testCompose1
 [Just 2,Nothing,Just 4]
->>> getCompose $ getCompose $ fmap (+1) $ Compose $ Compose [[Just 1,Nothing],[Just 3]]
+>>> testCompose2
 [[Just 2,Nothing],[Just 4]]
+>>> testCompose3
+[[Just 1,Just 2],[Just 3]]
+>>> testCompose4
+[[1,-1],[3]]
 -}
+-- nest level 1 to 1
+nest1 :: [Maybe Int]
+nest1 = [Just 1, Nothing, Just 3]
+testCompose1 :: [Maybe Int]
+testCompose1 = getCompose $ fmap (+1) $ Compose nest1
+-- nest level 2 to 2
+nest2 :: [[Maybe Int]]
+nest2 = [[Just 1,Nothing],[Just 3]]
+testCompose2 :: [[Maybe Int]]
+testCompose2 = getCompose $ getCompose $ fmap (+1) $ Compose $ Compose nest2
+-- lift up
+testCompose3 :: [[Maybe Int]]
+testCompose3 = getCompose $ fmap Just $ Compose [[1,2],[3]]
+-- lift down
+testCompose4 :: [[Int]]
+testCompose4 = getCompose $ fmap (fromMaybe (-1)) $ Compose nest2
 
 --
--- Pair
+-- Data.Functor.Product
 --
 {-|
 >>> (\(Pair x y) -> (x, y)) $ fmap (+1) $ Pair [1,2,3] (Just 123)
@@ -55,9 +76,10 @@ instance Functor (PA b) where
 --
 data Point x y = Point { pointX :: x, pointY :: y } deriving (Show, Eq)
 
-newtype PointXY a = PointXY (Point a a) deriving (Show, Eq)
-newtype PointX y x = PointX (Point x y) deriving (Show, Eq)
-newtype PointY x y = PointY (Point x y) deriving (Show, Eq)
+-- Wrapped types
+newtype PointXY a  = PointXY { unPointXY :: (Point a a) } deriving (Show, Eq)
+newtype PointX y x = PointX { unPointX :: (Point x y) } deriving (Show, Eq)
+newtype PointY x y = PointY { unPointY :: (Point x y) } deriving (Show, Eq)
 
 {-|
 prop> fmap id (PointXY (Point x y)) == id (PointXY (Point x y))
@@ -74,8 +96,72 @@ instance Functor (PointX y) where
 instance Functor (PointY x) where
   fmap f (PointY (Point x y)) = PointY $ Point x (f y)
 
+pointXYL :: [PointXY Int]
+pointXYL = [PointXY $ Point 1 2, PointXY $ Point 3 4]
+
 {-|
->>> getCompose $ fmap (+1) $ Compose [PointXY $ Point 1 2, PointXY $ Point 3 4]
-[PointXY (Point {pointX = 2, pointY = 3}),PointXY (Point {pointX = 4, pointY = 5})]
+>>> testFmapPointXY
+[PointXY {unPointXY = Point {pointX = 2, pointY = 3}},PointXY {unPointXY = Point {pointX = 4, pointY = 5}}]
 -}
-type PList_ x y = [Point x y]
+testFmapPointXY :: [PointXY Int]
+testFmapPointXY = (getCompose . fmap (+1) . Compose) pointXYL
+
+pointL :: [Point Int Int]
+pointL = [Point 1 2, Point 3 4]
+
+{-|
+>>> fmap (fmapX (+1)) pointL
+[Point {pointX = 2, pointY = 2},Point {pointX = 4, pointY = 4}]
+>>> fmap (fmapY (+1)) pointL
+[Point {pointX = 1, pointY = 3},Point {pointX = 3, pointY = 5}]
+>>> fmap (fmapXY (+1)) pointL
+[Point {pointX = 2, pointY = 3},Point {pointX = 4, pointY = 5}]
+>>> fmap (fmapY (*2)) $ fmap (fmapX (+1)) [Point 123 1.2 :: Point Int Double]
+[Point {pointX = 124, pointY = 2.4}]
+-}
+fmapX :: (a -> x) -> Point a y -> Point x y
+fmapX f = unPointX . fmap f . PointX
+fmapY :: (a -> y) -> Point x a -> Point x y
+fmapY f = unPointY . fmap f . PointY
+fmapXY :: (a -> b) -> Point a a -> Point b b
+fmapXY f = unPointXY . fmap f . PointXY
+
+-- Wrapped Tuple
+newtype FT1 b a = FT1 { getFT1 :: (a, b) } deriving (Show, Eq)
+instance Functor (FT1 b) where
+  fmap f (FT1 (a, b)) = FT1 (f a, b)
+newtype FT2 a b = FT2 { getFT2 :: (a, b) } deriving (Show, Eq)
+instance Functor (FT2 a) where
+  fmap f (FT2 (a, b)) = FT2 (a, f b)
+
+nest5 :: [(String, Maybe Bool)]
+nest5 = [("foo", Just True), ("bar", Nothing)]
+testCompose5 :: [(String, Maybe Bool)]
+testCompose5 = (getCompose . getCompose . fmap not . Compose . Compose) nest5
+
+nest6 :: [(String, Bool)]
+nest6 = [("foo", True), ("bar", False)]
+-- nmap6 :: [(a, b)] -> [(Maybe a, b)]
+nmap6 :: Functor f => (a -> a') -> f (a, b) -> f (a', b)
+nmap6 f = fmap getFT1 . getCompose . fmap f . Compose . fmap FT1
+testCompose6 :: [(Maybe String, Bool)]
+testCompose6 = nmap6 Just nest6
+-- nmap7 :: [(a, b)] -> [(a, Maybe b)]
+nmap7 :: Functor f => (b -> b') -> f (a, b) -> f (a, b')
+nmap7 f = fmap getFT2 . getCompose . fmap f . Compose . fmap FT2
+testCompose7 :: [(String, Maybe Bool)]
+testCompose7 = nmap7 Just nest6
+-- nmap8 :: [(a, b)] -> [(Maybe a, Maybe b)]
+nmap8 :: (forall t. t -> Maybe t) -> [(a, b)] -> [(Maybe a, Maybe b)]
+nmap8 f = nmap7 f . nmap6 f
+testCompose8 :: [(Maybe String, Maybe Bool)]
+testCompose8 = nmap8 Just nest6
+
+pair1 :: (String, Bool)
+pair1 = ("foo", True)
+
+newtype Wrapped f g a = Wrapped { getWrapped :: f (g a) } deriving Show
+
+-- Recursive fmap
+-- rfmap :: Functor f => (forall g. Functor g => g a -> g ) f a -> f b
+-- rfmap f = f . fmap f
