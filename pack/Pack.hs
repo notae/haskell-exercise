c40e27574c84443643f7d3ab91322cf88fb5b6b7
notae <notae@me.com>
2015-04-09 16:03:18 +0900

    Renamed.

--- pack/Pack.hs
+++ pack/Pack.hs
@@ -12,37 +12,54 @@ import           Data.Generics
 import           Data.Traversable    (Traversable)
 import qualified Data.Traversable    as Traversable
 
-class ContainerMap c where
-  cmap  :: (forall a. Data a => t a -> t' a) -> c t -> c t'
-  cmapA :: Applicative f =>
-           (forall a. Data a => t a -> f (t' a)) -> c t -> f (c t')
-  cmapM :: Monad m =>
-           (forall a. Data a => t a -> m (t' a)) -> c t -> m (c t')
-  cmapM f = unwrapMonad . cmapA (WrapMonad . f)
-  fromContainer :: (forall a. Data a => t a -> t') -> c t -> [t']
+class PackMap p where
+  pmap  :: (forall a. Data a => t a -> t' a) -> p t -> p t'
+  pmapA :: Applicative f =>
+           (forall a. Data a => t a -> f (t' a)) -> p t -> f (p t')
+  pmapM :: Monad m =>
+           (forall a. Data a => t a -> m (t' a)) -> p t -> m (p t')
+  pmapM f = unwrapMonad . pmapA (WrapMonad . f)
+  fromPack :: (forall a. Data a => t a -> t') -> p t -> [t']
 
-class ContainerLift c c' where
-  cup :: (forall a. a -> t a) -> c' -> c t
-  cdown :: (forall a. t a -> a) -> c t -> c'
+class PackLift p p' where
+  pup :: (forall a. a -> t a) -> p' -> p t
+  pdown :: (forall a. t a -> a) -> p t -> p'
 
--- | Container to hold data (variable domain, reference,
--- assignment, etc.) related to variables.
-class (ContainerMap c, ContainerLift c c') => Container c c'
+class (PackMap p, PackLift p p') => Pack p p'
 
--- | (for internal use)
-newtype CTraversable t' v t =
-  CTraversable { unCTraversable :: t' (t v) } deriving (Eq, Show)
+--
+-- Instances for Traversable
+--
+
+newtype PTraversable t' v t =
+  PTraversable { unPTraversable :: t' (t v) } deriving (Eq, Show)
 
 instance (Data v, Traversable t') =>
-         ContainerMap (CTraversable t' v) where
-  cmap f (CTraversable ts) = CTraversable $ fmap f ts
-  cmapA f (CTraversable ts) = CTraversable <$> Traversable.traverse f ts
-  fromContainer f (CTraversable ts) = Foldable.toList $ fmap f ts
+         PackMap (PTraversable t' v) where
+  pmap f (PTraversable ts) = PTraversable $ fmap f ts
+  pmapA f (PTraversable ts) = PTraversable <$> Traversable.traverse f ts
+  fromPack f (PTraversable ts) = Foldable.toList $ fmap f ts
 
-instance Traversable t' => ContainerLift (CTraversable t' v) (t' v) where
-  cup f ts = CTraversable $ fmap f ts
-  cdown f (CTraversable ts) = fmap f ts
+instance Traversable t' => PackLift (PTraversable t' v) (t' v) where
+  pup f ts = PTraversable $ fmap f ts
+  pdown f (PTraversable ts) = fmap f ts
 
-instance (ContainerMap (CTraversable t' v),
-          ContainerLift (CTraversable t' v) (t' v)) =>
-         Container (CTraversable t' v) (t' v)
+instance (PackMap (PTraversable t' v),
+          PackLift (PTraversable t' v) (t' v)) =>
+         Pack (PTraversable t' v) (t' v)
+
+
+--
+-- Examples
+--
+
+data Color = Red | Green | Blue deriving (Show, Eq)
+data D c n = D { dCol :: c, dNum :: n } deriving (Show, Eq)
+d :: D (Maybe Color) [Int]
+d = D (Just Green) [1..3]
+ns :: [Int]
+ns = (+1) <$> dNum d
+
+infixl 1 &
+(&) :: a -> (a -> b) -> b
+x & f = f x
