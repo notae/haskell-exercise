9c68f6ac17fb2551a2d017ee62112149a710b845
notae <notae@me.com>
2015-04-16 18:05:29 +0900

    Added examples for Lens/Prism.

--- lens/Basic.hs
+++ lens/Basic.hs
@@ -11,37 +11,40 @@ import Control.Applicative (Applicative)
 import Control.Lens.Basic
 
 infixl 0 ^.
+(^.) :: s -> Lens s t a b -> a
+(^.) = flip view
 
-(^.) :: Lens s t a b -> s -> a
-(^.) = view
+infixl 1 .~
+(.~) :: Lens s t a b -> b -> s -> t
+(.~) = set
+
+infixl 1 %~
+(%~) :: Lens s t a b -> (a -> b) -> s -> t
+(%~) = over
 
 data Person = Person String Int deriving Show
-_age :: Lens Person Person Int Int
-_age f (Person x a) = fmap (\b -> Person x b) (f a)
+name :: Lens Person Person String String
+name f (Person a x) = fmap (\b -> Person b x) (f a)
+age :: Lens Person Person Int Int
+age f (Person x a) = fmap (\b -> Person x b) (f a)
 
 data Book = Book String Person String deriving Show
-_title :: Lens Book Book String String
-_title f (Book a x y) = fmap (\b -> Book b x y) (f a)
+title :: Lens Book Book String String
+title f (Book a x y) = fmap (\b -> Book b x y) (f a)
+author :: Lens Book Book Person Person
+author f (Book x a y) = fmap (\b -> Book x b y) (f a)
 
-person :: Person
-person = Person "foo" 14
+person1 :: Person
+person1 = Person "foo" 14
 
-book :: Book
-book = Book "hoge" person "content"
+book1 :: Book
+book1 = Book "hoge" person1 "content"
 
 {-|
->>> test1
+>>> book1 ^. author . age
+14
+>>> (title .~ "moge") book1
 Book "moge" (Person "foo" 14) "content"
+>>> (author . name %~ reverse) book1
+Book "hoge" (Person "oof" 14) "content"
 -}
-test1 :: Book
-test1 = set _title "moge" book
-
-data P a = P String a deriving Show
-_val :: Lens (P s) (P t) s t
-_val f (P k v) = fmap (\b -> P k b) (f v)
-
-liftPV :: Applicative f => P a -> P (f a)
-liftPV = over _val pure
-
--- liftL :: Applicative f => Lens s t a b -> s -> t
--- liftL l = over l pure
