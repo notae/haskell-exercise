8014f6dc9b22344518c5ebb7d8b8fda73903eba8
notae <notae@me.com>
2015-05-26 02:44:34 +0900

    Modified to use RGB8 instead of RGB16 internally.

--- graphics/Graphics.hs
+++ graphics/Graphics.hs
@@ -29,13 +29,29 @@ doubleImageSimple src = dst where
   dst = generateImage f (w * 2) (h * 2)
   f x y = pixelAt src (x `div` 2) (y `div` 2)
 
-doubleImageFileAve :: String -> IO ()
-doubleImageFileAve path = do
+doubleImageFileBilinear :: String -> IO ()
+doubleImageFileBilinear path = do
   Right dimg <- readImage path
-  let img = convToImageRGB16 dimg
-  let img' = ImageRGB16 $ doubleImageAve img
+  let img = convToImageRGB8 dimg
+  let img' = ImageRGB8 $ doubleImageBilinear img
   let path' = "2xave_" ++ path
   savePngImage path' img'
+
+convToImageRGB8 :: DynamicImage -> Image PixelRGB8
+convToImageRGB8 dimg = case dimg of
+  ImageY8 _ -> error "ImageY8"
+  ImageY16 _ -> error "ImageY16"
+  ImageYF _ -> error "ImageYF"
+  ImageYA8 _ -> error "ImageYA8"
+  ImageYA16 _ -> error "ImageYA16"
+  ImageRGB8 img -> img
+  ImageRGB16 _ -> error "ImageRGB16"
+  ImageRGBF _ -> error "ImageRGBF"
+  ImageRGBA8 img -> pixelMap dropTransparency img
+  ImageRGBA16 _ -> error "ImageRGBA16"
+  ImageYCbCr8 img -> convertImage img
+  ImageCMYK8 _ -> error "ImageCMYK8"
+  ImageCMYK16 _ -> error "ImageCMYK16"
 
 convToImageRGB16 :: DynamicImage -> Image PixelRGB16
 convToImageRGB16 dimg = case dimg of
@@ -52,16 +68,15 @@ convToImageRGB16 dimg = case dimg of
   ImageYCbCr8 img -> promoteImage (convertImage img :: Image PixelRGB8)
   ImageCMYK8 _ -> error "ImageCMYK8"
   ImageCMYK16 _ -> error "ImageCMYK16"
-  -- _ -> error "unsupported colorspace"
 
-doubleImageAve :: (Pixel a, Integral (PixelBaseComponent a))
-               => Image a -> Image a
-doubleImageAve src = dst where
+doubleImageBilinear :: (Pixel a, Integral (PixelBaseComponent a))
+                    => Image a -> Image a
+doubleImageBilinear src = dst where
   w = imageWidth src
   h = imageHeight src
   n = 2
   dst = generateImage f (w * n) (h * n)
-  f x y | if x == 0 then traceShow (x, y) False else False = undefined
+  f x y | (x == 0) && traceShow (x, y) False = undefined
   f x y = fromMaybe showError p where
     showError = error $ "doubleImageAve: internal error: " ++
                 show ((x, y), (sx, sy), (sx11, sy11))
@@ -87,30 +102,14 @@ doubleImageAve src = dst where
 bilinear :: (Pixel a, Integral (PixelBaseComponent a))
          => Rational -> Rational -> a -> a -> a -> a -> a
 bilinear u v p q r s =
-  mixWith (f v) (mixWith (f u) p q) (mixWith (f u) r s) where
+  mixWith (mix v) (mixWith (mix u) p q) (mixWith (mix u) r s) where
 
 linear :: (Pixel a, Integral (PixelBaseComponent a))
          => Rational -> a -> a -> a
-linear u p q = mixWith (f u) p q
+linear u p q = mixWith (mix u) p q
 
-f t _ x y = floor (fromIntegral x * (1 - t) + fromIntegral y * t)
+mix :: Integral a => Rational -> t -> a -> a -> a
+mix t _ x y = floor (fromIntegral x * (1 - t) + fromIntegral y * t)
 
 itor :: Int -> Rational
 itor = fromInteger . toInteger
-
-type PosX = Int
-type PosY = Int
-type PosDiff = Int
-type Weight = Int
-
-{-
-resolvePixel :: Pixel a
-             => Image a -> PosX -> PosY
-             -> [(PosDiff, PosDiff, Weight)] -> a
-resolvePixel i x y t ds = s / t where
-  s = foldl' f  ds where
-    f
-
-mixPixels :: Pixel a => Image a -> Int -> Int -> Int -> [(Int, Int, Int)] -> a
-mixPixels i x y t ds = s / t where
--}
