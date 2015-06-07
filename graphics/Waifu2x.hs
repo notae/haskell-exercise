3d5cf9ccb9f47ee8469c26b9f8a8e4f03fae0ecc
notae <notae@me.com>
2015-06-08 00:13:03 +0900

    Added main process. (not work yet)

--- graphics/Waifu2x.hs
+++ graphics/Waifu2x.hs
@@ -16,12 +16,13 @@ import           Control.Applicative
 import qualified Data.ByteString.Lazy as B
 import           Data.Data            (Data, Typeable, toConstr)
 import           Data.Either
+import           Data.List            (foldl', foldl1')
 import           Debug.Trace          (traceShow)
 import           GHC.Generics         (Generic)
 import           System.Environment   (getArgs)
 
 import Codec.Picture
-import Codec.Picture.Metadata
+import Codec.Picture.Metadata (Metadatas)
 import Codec.Picture.Types
 import Control.Lens
 import Data.Aeson
@@ -48,12 +49,13 @@ data Step =
   { _nInputPlane  :: Int
   , _nOutputPlane :: Int
   , _weight       :: [[Kernel]]   -- ^ nOutputPlane * nInputPlane * (3*3)
-  , _bias         :: [Float]      -- ^ nOutputPlane
+  , _bias         :: [Bias]      -- ^ nOutputPlane
   , _kW           :: Int
   , _kH           :: Int
   } deriving (Show, Generic)
 
 type Kernel = [[Float]]
+type Bias = Float
 
 deriveJSON defaultOptions{fieldLabelModifier = drop 1} ''Step
 
@@ -75,6 +77,7 @@ dumpStep step = do
   dumpTitle "Step"
   dump "nInputPlane" (step ^. nInputPlane)
   dump "nOutputPlane" (step ^. nOutputPlane)
+  dump "kW, kH" (step ^. kW, step ^. kH)
 
 --
 -- Image
@@ -83,6 +86,7 @@ dumpStep step = do
 toImageYCbCr8 :: DynamicImage -> Maybe (Image PixelYCbCr8)
 toImageYCbCr8 dimg = case dimg of
   ImageYCbCr8 img -> Just img
+  ImageRGB8 img -> Just $ convertImage img
   _ -> Nothing
 
 doubleImageNN :: Pixel a => Image a -> Image a
@@ -91,6 +95,31 @@ doubleImageNN src = dst where
   h = imageHeight src
   dst = generateImage f (w * 2) (h * 2)
   f x y = pixelAt src (x `div` 2) (y `div` 2)
+
+padEdge :: Pixel a => Int -> Image a -> Image a
+padEdge n img = img' where
+  (w, h) = getImageSize img
+  img' = generateImage f (w + n * 2) (h + n * 2)
+  f x y = pixelAt img x' y' where
+    x' = clamp n (n + w - 1) x - n
+    y' = clamp n (n + h - 1) y - n
+
+{-|
+>>> fmap (clamp 2 5) [0, 3, 7]
+[2,3,5]
+-}
+clamp :: Ord a => a -> a -> a -> a
+clamp l h = min h . max l
+
+getImageSize :: Image a -> (Int, Int)
+getImageSize dimg = (w, h) where
+  w = imageWidth  dimg
+  h = imageHeight dimg
+
+getDynamicImageSize :: DynamicImage -> (Int, Int)
+getDynamicImageSize dimg = (w, h) where
+  w = dynamicMap imageWidth  dimg
+  h = dynamicMap imageHeight dimg
 
 --
 -- Waifu2x Core
@@ -106,7 +135,8 @@ waifu2x model dimg = do
   return $ ImageYCbCr8 img'
 
 checkModel :: Model -> Either String Model
-checkModel model = checkLength model >>= checkNOutputPlane where
+checkModel model = checkLength model >>= checkNOutputPlane
+  where
   checkLength m =
     if l > 1
     then Right m
@@ -117,20 +147,64 @@ checkModel model = checkLength model >>= checkNOutputPlane where
     then Right m
     else Left $ "invalid number of output planes: " ++ show nop
     where nop = last m ^. nOutputPlane
+  -- TBD: more check
+
+type Plane = Image PixelF
 
 waifu2xMain :: Model -> Image PixelYCbCr8 -> Image PixelYCbCr8
 waifu2xMain model img = img' where
+  -- pre-process
   img2x = doubleImageNN img
-  yf, yf' :: Image PixelF
+  yf :: Plane
   yf = promoteImage (extractLumaPlane img2x)
-  -- TBD: process yf with model
-  yf' = yf
+  planes0 :: [Plane]
+  planes0 = [padEdge (length model) yf]
+  count = sum [step ^. nInputPlane * step ^. nOutputPlane | step <- model]
+  -- TBD: progress in StateT
+
+  -- main process
+  yf' :: [Plane]
+  yf' = foldl' procStep planes0 model where
+    procStep :: [Plane] -> Step -> [Plane]
+    procStep inPlanes step =
+      zipWith procOutPlane (step ^. weight) (step ^. bias) where
+        procOutPlane :: [Kernel] -> Bias -> Plane
+        procOutPlane ws b = cutNeg . addBias b . sumP $
+                            zipWith convolute ws inPlanes
+
+  -- plane operations
+  cutNeg :: Plane -> Plane
+  cutNeg = pixelMap $ \y -> max y 0 + 0.1 * min y 0
+  convolute :: Kernel -> Plane -> Plane
+  convolute k p = p' where
+    (w, h) = getImageSize p
+    (w', h') = (w - 2, h - 2)
+    p' = generateImage f w' h'
+    f'' x y = pixelAt p (x+1) (y+1)
+    f' x y = traceShow (x, y, f x y) (f x y)
+    f x y = sum $ fmap gx (zip k [0..]) where
+      gx :: ([PixelF], Int) -> PixelF
+      gx (kl, kx) = sum $ fmap gy (zip3 kl (repeat kx) [0..]) where
+      gy :: (PixelF, Int, Int) -> PixelF
+      -- gy (kp, kx, ky) | traceShow (kp, kx, ky) False = undefined
+      gy (kp, kx, ky) = pixelAt p (x+kx) (y+ky) * kp
+  sumP :: [Plane] -> Plane
+  sumP [] = error "sumP: empty list"
+  sumP ps@(p0:_) = s where
+    (w, h) = getImageSize p0
+    s = generateImage f w h
+    f x y = sum (fmap (\p -> pixelAt p x y) ps) /
+            (fromInteger . toInteger . length) ps
+  addBias :: Float -> Plane -> Plane
+  addBias b = pixelMap (+b)
+
+  -- post-process
   y8' :: Image (PixelBaseComponent PixelYCbCr8)
-  y8' = pixelMap (\y -> ceiling (y * 255)) yf'
+  y8' = pixelMap (clamp 0 255 . floor . (* 255)) (head yf')
   img' = pixelMapXY f img2x where
     f :: Int -> Int -> PixelYCbCr8 -> PixelYCbCr8
     f x y p = setY p (pixelAt y8' x y)
-    setY :: PixelYCbCr8 -> (PixelBaseComponent PixelYCbCr8) -> PixelYCbCr8
+    setY :: PixelYCbCr8 -> PixelBaseComponent PixelYCbCr8 -> PixelYCbCr8
     setY (PixelYCbCr8 _ cb cr) py = PixelYCbCr8 py cb cr
 
 --
@@ -152,8 +226,9 @@ convMain mPath iPath oPath = do
 dumpImageInfo :: String -> DynamicImage -> Metadatas -> IO ()
 dumpImageInfo title dimg md = do
   dumpTitle title
-  dump "width" (dynamicMap imageWidth dimg)
-  dump "height" (dynamicMap imageHeight dimg)
+  let (w, h) = getDynamicImageSize dimg
+  dump "width" w
+  dump "height" h
   dump "image type" (showImageType dimg)
   dump "metadata" md
 
