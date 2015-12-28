9ff689d9c86ee642f05970e611cfa03f4ef81c08
notae <notae@me.com>
2015-12-29 01:25:57 +0900

    Updated sbv version.

--- cp/SBVTest.hs
+++ cp/SBVTest.hs
@@ -20,16 +20,16 @@ pyt n (x, y, z) =
 {-|
 >>> allSatPyt 15
 Solution #1:
-  s0 = 9 :: Integer
+  s0 =  9 :: Integer
   s1 = 12 :: Integer
   s2 = 15 :: Integer
 Solution #2:
-  s0 = 5 :: Integer
+  s0 =  5 :: Integer
   s1 = 12 :: Integer
   s2 = 13 :: Integer
 Solution #3:
-  s0 = 6 :: Integer
-  s1 = 8 :: Integer
+  s0 =  6 :: Integer
+  s1 =  8 :: Integer
   s2 = 10 :: Integer
 Solution #4:
   s0 = 3 :: Integer
