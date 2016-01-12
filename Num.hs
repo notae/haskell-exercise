382d26daccbee21faad6d538f2a6b316ba16b8c1
notae <notae@me.com>
2016-01-13 02:28:35 +0900

    Add another style with type class Num.

--- Num.hs
+++ Num.hs
@@ -10,9 +10,9 @@ data Expr
   deriving (Show, Read, Eq)
 
 instance Num Expr where
+  fromInteger i = Value (fromInteger i)
   a + b = a :+ b
   a * b = a :* b
-  fromInteger i = Value (fromInteger i)
 
 {-|
 >>> eval [("x", 10), ("y", 3)] (Var "x" + Var "y" * 2)
@@ -23,3 +23,27 @@ eval _ (Value i) = i
 eval e (Var s) = fromMaybe 0 (lookup s e)
 eval e (x :+ y) = eval e x + eval e y
 eval e (x :* y) = eval e x * eval e y
+
+
+ex1 :: Num a => a
+ex1 = (1 + 2) * 3
+
+{-|
+>>> unS $ ex1
+"(1+2)*(3)"
+-}
+newtype ExprS = S { unS :: String }
+instance Num ExprS where
+  fromInteger i = S $ show i
+  S a + S b = S $ a ++ "+" ++ b
+  S a * S b = S $ "(" ++ a ++ ")" ++ "*" ++ "(" ++ b ++ ")"
+
+{-|
+>>> unV $ ex1
+9
+-}
+newtype ExprV = V { unV :: Int }
+instance Num ExprV where
+  fromInteger i = V $ fromInteger i
+  V a + V b = V $ a + b
+  V a * V b = V $ a * b
