cd6669a33b9951a7c8c0873373d31a1893c2dbca
notae <notae@me.com>
2016-01-13 22:10:42 +0900

    Fix to use Identity.

--- cp/SBVTest3.hs
+++ cp/SBVTest3.hs
@@ -1,105 +1,29 @@
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE StandaloneDeriving         #-}
 
 module SBVTest3 where
 
-import GHC.Exts
+import Data.Functor.Identity
 
 import Data.SBV
-
-class Symantics repl where
-  type Ctx repl a :: Constraint
-  int :: Ctx repl i => i -> repl i
-  add :: Ctx repl i => repl i -> repl i -> repl i
-  eq  :: Eq i => repl i -> repl i -> repl Bool
-  lam :: (repl a -> repl b) -> repl (a -> b)
-  app :: repl (a -> b) -> repl a -> repl b
-
-type VarCounter = Int
-newtype S a = S { unS :: VarCounter -> String }
-
-instance Symantics S where
-  type Ctx S a = Show a
-  int x     = S $ const $ show x
-  add e1 e2 = S $ \h ->
-    "(" ++ unS e1 h ++ "+" ++ unS e2 h ++ ")"
-  eq e1 e2  = S $ \h ->
-    "(" ++ unS e1 h ++ "=" ++ unS e2 h ++ ")"
-
-  lam e = S $ \h ->
-    let x = "x" ++ show h
-    in "(\\" ++ x ++ " -> " ++
-       unS (e (S $ const x)) (succ h) ++ ")"
-  app e1 e2 = S $ \h ->
-    "(" ++ unS e1 h ++ " " ++ unS e2 h ++ ")"
-
-newtype V a = V { unV :: a } deriving (Show, Num)
-
-instance Symantics V where
-  type Ctx V a = Num a
-  int x     = V x
-  add e1 e2 = V $ unV e1 + unV e2
-  eq e1 e2  = V $ unV e1 == unV e2
-  lam f     = V $ unV . f . V
-  app e1 e2 = V $ (unV e1) (unV e2)
-
-instance Symantics SBV where
-  type Ctx SBV a = (Num a, SymWord a)
-  int x     = literal x
-  add e1 e2 = e1 + e2
-  eq e1 e2  = e1 .== e2
---   lam f     = R $ unR . f . R
---   app e1 e2 = R $ (unR e1) (unR e2)
-
--- predicate in tag-less final representation
-p :: (Symantics repl, Ctx repl Word8) => repl Word8 -> repl Bool
-p x = (int 2 `add` x) `eq` int 5
-
-{-|
-print
->>> s
-"((2+3)=5)"
--}
-s :: String
-s = unS (p (int 3)) 0
-
-{-|
-solve as constraints
->>> r
-Solution #1:
-  s0 = 3 :: Word8
-This is the only solution.
--}
-r :: IO AllSatResult
-r = allSat (p :: SWord8 -> SBool)
-
-{-|
-evaluate as Haskell expression
->>> v
-True
--}
-v :: Bool
-v = unV $ p (int 3)
-
 
 class Eq2 repl where
   infix 4 @==, @/=
   (@==) :: Eq a => repl a -> repl a -> repl Bool
   (@/=) :: Eq a => repl a -> repl a -> repl Bool
 
-instance Eq2 V where
-  V x @== V y = V $ x == y
-  V x @/= V y = V $ x /= y
+instance Eq2 Identity where
+  (@==) = \x y -> (==) <$> x <*> y
+  (@/=) = \x y -> (/=) <$> x <*> y
 
 instance Eq2 SBV where
   (@==) = (.==)
   (@/=) = (./=)
 
+deriving instance Num a => Num (Identity a)
+
 {-|
->>> unV $ p2 3
+>>> runIdentity $ p2 3
 True
 >>> allSat (p2 :: SWord8 -> SBool)
 Solution #1:
@@ -108,3 +32,31 @@ This is the only solution.
 -}
 p2 :: (Num (repl Word8), Eq2 repl) => repl Word8 -> repl Bool
 p2 x = 2 + x @== 5
+
+{-|
+Apply as normal Haskell function:
+>>> runIdentity $ p3 2 3
+True
+>>> runIdentity $ p3 3 3
+False
+-}
+p3 :: (Eq a, Num (repl a), Eq2 repl) => repl a -> repl a -> repl Bool
+p3 x y = x + y @== 5
+
+{-|
+Apply as SBV predicate:
+>>> allSat p4
+Solution #1:
+  s0 = 1 :: Word8
+  s1 = 4 :: Word8
+Solution #2:
+  s0 = 2 :: Word8
+  s1 = 3 :: Word8
+Found 2 different solutions.
+-}
+p4 :: SWord8 -> SWord8 -> Symbolic SBool
+p4 x y = do
+  constrain $ x `inRange` (1, 3)
+  constrain $ y `inRange` (3, 5)
+  constrain $ p3 x y
+  return true
