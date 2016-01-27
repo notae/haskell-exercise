d92f9bbf71c5dfcb37fadb384572858a34b6346a
notae <notae@me.com>
2016-01-28 01:21:45 +0900

    Support type constraints.

--- pack/Recursive.hs
+++ pack/Recursive.hs
@@ -1,7 +1,12 @@
-{-# LANGUAGE DefaultSignatures #-}
-{-# LANGUAGE KindSignatures    #-}
-{-# LANGUAGE RankNTypes        #-}
-{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE DefaultSignatures     #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE KindSignatures        #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE UndecidableInstances  #-}
 
 module Recursive where
 
@@ -12,25 +17,49 @@ import           Data.Set (Set)
 import qualified Data.Set as Set
 
 
+{-|
+Lift to 'Applicative':
+
+>>> plift pure (1::Int, True) :: ([Int], [Bool])
+([1],[True])
+
+Lift to non-'Applicative':
+
+>>> plift Set.singleton (1::Int, True) :: (Set Int, Set Bool)
+(fromList [1],fromList [True])
+
+>>> pliftM (Identity . pure) (1::Int, True) :: Identity ([Int], [Bool])
+Identity ([1],[True])
+
+>>> punlift head ([1], [True]) :: (Int, Bool)
+(1,True)
+
+>>> punliftM (Identity . head) ([1::Int], [True]) :: Identity (Int, Bool)
+Identity (1,True)
+
+>>> psequence ([1, 2], [True, False]) :: [(Int, Bool)]
+[(1,True),(1,False),(2,True),(2,False)]
+-}
+
 class P p where
   type L p (f :: * -> *)
   plift :: (forall a. a -> f a) -> p -> L p f
   pliftM :: Monad m => (forall a. a -> m (f a)) -> p -> m (L p f)
-  pdown :: (forall a. f a -> a) -> L p f -> p
-  pdownM :: Monad m => (forall a. f a -> m a) -> L p f -> m p
-  pdown' :: Applicative f => L p f -> f p
+  punlift :: (forall a. f a -> a) -> L p f -> p
+  punliftM :: Monad m => (forall a. f a -> m a) -> L p f -> m p
+  psequence :: Applicative f => L p f -> f p
   -- Default implementation
   type L p f = f p
   default plift :: (forall a. a -> f a) -> p -> f p
   default pliftM :: Monad m => (forall a. a -> m (f a)) -> p -> m (f p)
-  default pdown :: (forall a. f a -> a) -> f p -> p
-  default pdownM :: Monad m => (forall a. f a -> m a) -> f p -> m p
-  default pdown' :: Applicative f => f p -> f p
+  default punlift :: (forall a. f a -> a) -> f p -> p
+  default punliftM :: Monad m => (forall a. f a -> m a) -> f p -> m p
+  default psequence :: Applicative f => f p -> f p
   plift f a = f a
   pliftM f a = f a
-  pdown f a = f a
-  pdownM f a = f a
-  pdown' a = a
+  punlift f a = f a
+  punliftM f a = f a
+  psequence a = a
 
 -- Using default implementation
 instance P Bool
@@ -40,48 +69,67 @@ instance (P a, P b) => P (a, b) where
   type L (a, b) f = (L a f, L b f)
   plift f (a, b) = (plift f a, plift f b)
   pliftM f (a, b) = (,) <$> pliftM f a <*> pliftM f b
-  pdown f (a, b) = (pdown f a, pdown f b)
-  pdownM f (a, b) = (,) <$> pdownM f a <*> pdownM f b
-  pdown' (a, b) = (,) <$> pdown' a <*> pdown' b
+  punlift f (a, b) = (punlift f a, punlift f b)
+  punliftM f (a, b) = (,) <$> punliftM f a <*> punliftM f b
+  psequence (a, b) = (,) <$> psequence a <*> psequence b
+
+
+newtype Lift f c = Lift (forall a. c a => a -> f a)
+newtype LiftM f c m = LiftM (forall a. c a => a -> m (f a))
+newtype Unlift f c = Unlift (forall a. c a => f a -> a)
+newtype UnliftM f c m = UnliftM (forall a. c a => f a -> m a)
 
 {-|
->>> testPLift
-([1],[True])
--}
-testPLift :: ([Int], [Bool])
-testPLift = plift pure (1::Int, True)
+Lift to non-'Applicative' with type context ('Ord'):
 
-{-|
->>> testPLiftWithCtx
+>>> plift' ((Lift $ \x -> Set.fromList [x]) :: Lift Set Ord) (1::Int, True) :: (Set Int, Set Bool)
 (fromList [1],fromList [True])
--}
-testPLiftWithCtx :: (Set Int, Set Bool)
-testPLiftWithCtx = plift Set.singleton (1::Int, True)
 
-{-|
->>> testPLiftM
-Identity ([1],[True])
--}
-testPLiftM :: Identity ([Int], [Bool])
-testPLiftM = pliftM (Identity . pure) (1::Int, True)
+>>> pliftM' ((LiftM $ Identity . Set.fromList . (:[])) :: LiftM Set Ord Identity) (1::Int, True) :: Identity (Set Int, Set Bool)
+Identity (fromList [1],fromList [True])
 
-{-|
->>> testPDown
+>>> punlift' (Unlift minimum :: Unlift [] Ord) ([1::Int], [True]) :: (Int, Bool)
 (1,True)
--}
-testPDown :: (Int, Bool)
-testPDown = pdown head ([1], [True])
 
-{-|
->>> testPDownM
+>>> punliftM' ((UnliftM $ Identity . minimum) :: UnliftM [] Ord Identity) ([1::Int], [True]) :: Identity (Int, Bool)
 Identity (1,True)
 -}
-testPDownM :: Identity (Int, Bool)
-testPDownM = pdownM (Identity . head) ([1::Int], [True])
 
-{-|
->>> testPDown'
-[(1,True),(1,False),(2,True),(2,False)]
--}
-testPDown' :: [(Int, Bool)]
-testPDown' = pdown' ([1, 2], [True, False])
+class P' p c where
+  plift' :: Lift f c -> p -> L p f
+  pliftM' :: Monad m => LiftM f c m -> p -> m (L p f)
+  punlift' :: Unlift f c -> L p f -> p
+  punliftM' :: Monad m => UnliftM f c m -> L p f -> m p
+  -- Default implementation
+  default plift' :: c p => Lift f c -> p -> f p
+  default pliftM' :: (c p, Monad m) => LiftM f c m -> p -> m (f p)
+  default punlift' :: c p => Unlift f c -> f p -> p
+  default punliftM' :: (c p, Monad m) => UnliftM f c m -> f p -> m p
+  plift' (Lift f) a = f a
+  pliftM' (LiftM f) a = f a
+  punlift' (Unlift f) a = f a
+  punliftM' (UnliftM f) a = f a
+
+-- Using default implementation
+instance c Bool => P' Bool c
+instance c Int => P' Int c
+
+instance (P' a c, P' b c) => P' (a, b) c where
+  plift' f (a, b) = (plift' f a, plift' f b)
+  pliftM' f (a, b) = (,) <$> pliftM' f a <*> pliftM' f b
+  punlift' f (a, b) = (punlift' f a, punlift' f b)
+  punliftM' f (a, b) = (,) <$> punliftM' f a <*> punliftM' f b
+
+
+infixr 0 ~~>
+type (f ~~> g) c = forall a. c a => f a -> g a
+
+infixr 0 :~~>, $$$
+newtype (f :~~> g) c = CNat { ($$$) :: (f ~~> g) c }
+
+
+infixr 0 ~->
+type (f ~-> g) c m = forall a. c a => f a -> m (g a)
+
+infixr 0 :~->, $-
+newtype (f :~-> g) c m = CNT { ($-) :: (f ~-> g) c m }
