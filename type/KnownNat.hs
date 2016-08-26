4a23c3bfc530bebfe8b5a61ad41d0e50626b9820
notae <notae@me.com>
2016-08-26 18:35:08 +0900

    Add doctest.

--- type/KnownNat.hs
+++ type/KnownNat.hs
@@ -8,6 +8,13 @@ module KnownNat where
 import Data.Proxy
 import GHC.TypeLits
 
+{- $setup
+>>> :set -XDataKinds
+-}
+
+{-|
+>>> f (Proxy :: Proxy 1)
+3
+-}
 f :: forall n m. (KnownNat n, KnownNat m, m ~ (n + 2)) => Proxy n -> Integer
-f _ = natVal (Proxy :: Proxy n) +
-      natVal (Proxy :: Proxy m)
+f _ = natVal (Proxy :: Proxy m)
